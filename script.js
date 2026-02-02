2265bdaa15ffa13ce14be22b6d45d570f8f10801
Davi <davilimasoares1@outlook.com>
2026-02-01 22:14:14 -0300

    I change the 'myLibrary[i]' to bookObj

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function addBookToLibrary(name1, author1, pages1, read1) {
     book.querySelector('.read').textContent = `Read: ${bookObj.read}`;
 
     const cardDiv = book.querySelector('.book-card');
-    book.id = myLibrary[i].id;
+    book.id = bookObj.id;
     cardDiv.dataset.id = a;
 
     const bookDelete = book.querySelector('.delete-card');
